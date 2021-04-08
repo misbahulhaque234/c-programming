6e88e3dfe3574b5e308e3fc6644d72b333b9cfdf
misbahulhaque <misbahulhaque234@gmail.com>
2021-04-09 00:26:56 +0530

    Initial Commit

---
+++ 1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+    printf("I'm Misbahul Haque. I'm from Bihar Shafir Nalanda\n");
+    printf("this is my 1st 'c' coding\n");
+    return 0;
+}
